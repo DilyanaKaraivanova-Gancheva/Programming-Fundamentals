8341b5813e9eee45057984be11e080551d4379d0
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-02-21 10:23:03 +0200

    Add files via upload

---
+++ Arrays-Excersises/09. Index of Letters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Index_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word = Console.ReadLine().ToLower().ToCharArray();
+            //char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+            //'q','r','s','t','u','v','w','x','y','z'};
+
+            int counter = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                counter = (word[i] - 97);
+
+                Console.WriteLine($"{word[i]} -> {counter}");
+            }
+        }
+    }
+}
---
+++ Arrays-Excersises/10. Pairs by Difference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Pairs_by_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] - numbers[j] == difference)
+                    {
+                        counter++;
+                    }
+
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
