7ee04be3321f9f5b2c3b5208c77e1279825d0e0c
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-02-21 10:35:51 +0200

    Add files via upload

---
+++ Arrays - LAB/02. Reverse Array of Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Reverse_Array_of_Integers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n =int.Parse(Console.ReadLine());
+			
+			int[] arr =new int[n];
+
+			for (int i = 0; i < n ; i++)
+			{
+				arr[i] = int.Parse(Console.ReadLine());
+			}
+			for (int i = n-1; i >= 0; i--)
+			{
+				Console.Write(arr[i]+" ");
+			}
+			
+			//SecondWay
+			//int[] arr = values.Select(int.Parse).ToArray();
+
+			//ThirdWay
+
+		}
+	}
+}
