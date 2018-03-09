11a287045667baec335817b91f5ae3130870af28
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-03-09 12:28:50 +0200

    Add files via upload

---
+++ List Exercises/06. Sum Reversed Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Sum_Reversed_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+
+            List<int> reverstNums = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                char[] charTo = numbers[i].ToCharArray();
+                char[] reversedChars = charTo.Reverse().ToArray();
+
+                reverstNums.Add(int.Parse(string.Join("",reversedChars)));
+            }
+
+            Console.WriteLine(reverstNums.Sum());
+        }
+    }
+}
---
+++ List Exercises/07. Bomb Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)  // 80/100
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int[] detonators = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int bomb = detonators[0];
+            int pow = detonators[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bomb)
+                {
+                    if ((i-pow)>= 0 && (2*pow + 1) <= numbers.Count-1)
+                    {
+                        numbers.RemoveRange(Math.Max(i - pow, 0),Math.Min(2 * pow + 1,numbers.Count - 1));
+                    }
+                    else if ((i-pow) < 0 && (2 * pow + 1) <= numbers.Count - 1)
+                    {
+                        numbers.RemoveRange(0, i + pow);
+                    }
+                    else if ((i - pow) >= 0 && (2*pow + 1) > numbers.Count -1)
+                    {
+                        numbers.RemoveRange(i - pow, (numbers.Count - (i - pow)));
+                    }
+                    else
+                    {
+                        numbers.RemoveRange(0,numbers.Count);
+                    }
+                    i--;
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
